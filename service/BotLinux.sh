d483f8e1afb02b99f389b16ecf52f060a4e685ae
don <doncezario.101@gmail.com>
2022-04-29 01:00:05 +0300

    Final commit

--- service/BotLinux.sh
+++ service/BotLinux.sh
@@ -1,6 +1,7 @@
 cd ..
 source "venv/bin/activate"
 pip install -r "service/requirements.txt"
+clear
 python "main.py"
 deactivate
 cd "service" || exit
--- service/BotWindows.bat
+++ service/BotWindows.bat
@@ -2,5 +2,6 @@
 CALL ..\venv\Scripts\activate.bat
 pip install -r requirements.txt
 cd ..
+cls
 python main.py
 pause
